c2b6770e8811f1a7e5116edf704cefd2b9100224
Oliver Kandén <oliver.kanden04@gmail.com>
2026-01-06 19:25:34 +0200

    Playing with the API done

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,18 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # A Question has a question and a publication date.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 # A Choice has two fields: the text of the choice and a vote tally.
 # Each Choice is associated with a Question.
@@ -13,3 +21,6 @@ class Choice(models.Model):
     # ForeignKey tells Django each Choice is related to a single Question.
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text

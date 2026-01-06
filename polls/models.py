32ebd6b5a0a6e90fab3855eca35e7633f92c9b35
Oliver Kandén <oliver.kanden04@gmail.com>
2026-01-06 19:04:24 +0200

    Creating models done

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# A Question has a question and a publication date.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+# A Choice has two fields: the text of the choice and a vote tally.
+# Each Choice is associated with a Question.
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # ForeignKey tells Django each Choice is related to a single Question.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)

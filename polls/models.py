f10c06a86175a498235965f81172a97f30fbd018
Oliver Kandén <oliver.kanden04@gmail.com>
2026-01-07 14:51:56 +0200

    Fixing the bug done

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
 
 # A Choice has two fields: the text of the choice and a vote tally.
 # Each Choice is associated with a Question.

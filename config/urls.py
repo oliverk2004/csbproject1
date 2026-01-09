0ac0a664f53cf81a4a952ce9d43a08c39c505e54
oliverk2004 <oliver.kanden04@gmail.com>
2026-01-09 14:41:07 +0200

    Login works

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from polls.views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
